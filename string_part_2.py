1953ca3c6e76b4441813971338fa6b86cb80f859
rohanravula <109545469+rohanravula@users.noreply.github.com>
2022-07-23 13:58:05 +0530

    Add files via upload

---
+++ string_part_2.py
@@ -0,0 +1,85 @@
+# a=8
+# b=6.8
+# c='hello'
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# a='hey' ; b='bye'
+# print(a+b)
+# print(a,b)
+# print(a+'  '+b) #this is cancatenation method
+
+# a='hello' ; b=5
+# print(a+'  '+b) #if we try to add str+int it shows type
+
+"Type convernation and type casting (how to convert int to str)"
+
+# a="hi" ; b="bye" ; c=5 ; d=4.6
+# print(a+str(c))
+# print(b+str(c))
+# print(b+str(d))
+# print(b+'    '+str(d)) #this how we can give space also. by using the cancatenation menthod
+
+'it is called convertion int to float, float to int'
+from turtle import bye
+
+
+a=4 ; b=5.8 ; c="hello"
+# print(int(b)) #5
+# print(float(a)) #4.0
+# print(int(c)) #we cant convert the str to int or float it shows the type error
+
+'how to conver the str to int & float, when str hold a number'
+# x='10' #it in str type but the nuber is int
+# print(int(x)) #10
+# print(float(x)) #10.0
+
+# y="4.8" #it is in str type but the number is float
+# print(int(y)) #we cant convert the float value directly to int
+# print(float(y)) #we can conver the float value directly to float
+
+# a=float(y) #process to convert float vlaue into int
+# print(int(a))
+
+# y='9.0'
+# print(float(y))
+
+
+a=7;b=7.0; c="hey" ;d=5;e=3.9;f="bye"
+# print(a+d) #possible
+# print(a+b) #possible
+# print(a+f) #not possible
+# print(b+d) #possible
+# print(b+e) #possible
+# print(f+e) #not possible
+# print(c+f) #possible
+
+# print('hi'  'bye') #hibye
+# print('python') ; print('java') #in this process the answer will dispaly in multiple line.
+# print('h' , 'i') #h i
+# print('h' 'i') #hi
+# print('hi'
+# 'bye') #hibye 
+# print('hi'
+
+# 'rohan') #hirohan
+# print('hi  '                             'python')  #hi python
+# print('hi how are you, how do you do \
+# whats up')
+# print('''hii , this is roha'n
+# and whats
+#     your name 
+# ma'y i know''') #this acts as xerox 
+# print("""hi how are are yore" you""") #acts as a xerox    
+# print('hi')
+# print()
+# print('bye')
+# print('hi'+'bye')
+# print('hi'*5) #the  output will be withut space
+# print('hi '*5) #with space the out it will represent
+
+# print('6.7'  *3) #a str can be * by only int
+# print('helo' *5.0) #a str cant be * by float
+# print('rohan' * 'kumar') #a str cant be * by anyother string
+print('       '+'hi')
